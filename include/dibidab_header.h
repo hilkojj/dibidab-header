f98e91aceb8d0ebed2b1e66057f128e3940e05d4
hilkojj <hilkojj@outlook.com>
2024-10-30 21:53:42 +0100

    Thanks MSVC

--- include/dibidab_header.h
+++ include/dibidab_header.h
@@ -11,7 +11,7 @@
  * Choose where to expose the following struct members to.
  * Options are: lua, json
  */
-#define dibidab_expose(types...)
+#define dibidab_expose(...)
 
 /**
  * Generate code for using this struct as a component.
